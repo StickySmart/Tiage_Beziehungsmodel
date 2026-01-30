993a4343b4ff6eef82294208c32933686341496e
StickySmart <Info@ti-age.de>
2026-01-30 16:20:45 +0100

    style: add spacer for symmetric DU/PARTNER alignment

--- service-worker.js
+++ service-worker.js
@@ -3,8 +3,8 @@
  * Version: 1.1.0 - Erweiterte Precache-Liste
  */
 
-const CACHE_NAME = 'tiage-v14';
-const STATIC_CACHE_NAME = 'tiage-static-v14';
+const CACHE_NAME = 'tiage-v15';
+const STATIC_CACHE_NAME = 'tiage-static-v15';
 
 // Kritische Ressourcen die sofort gecacht werden
 const PRECACHE_URLS = [
